52bd05c14192d3faf4e248360d10284b1df2308e
Saikat Mallick <saikat.mallick@nuagebiz.tech>
2026-08-21 13:22:34 +0530

    state locking

--- Module/VPC/variables.tf
+++ Module/VPC/variables.tf
@@ -22,3 +22,13 @@ variable "private_subnets" {
     description = "Private subnets for vpc cidr for test"
     type = list(string)
 }
+
+variable "s3_bucket_name" {
+  description = "S3 bucket name for Terraform state"
+  type        = string
+}
+
+variable "dynamodb_table_name" {
+  description = "DynamoDB table name for Terraform state lock"
+  type        = string
+}
--- environment/dev/providers.tf
+++ environment/dev/providers.tf
@@ -5,7 +5,17 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket         = var.s3_bucket_name
+    key            = "dev/terraform.tfstate"
+    region         = var.aws_region
+    dynamodb_table = var.dynamodb_table_name
+  }
+
 }
+
+
 
 provider "aws" {
   region = var.aws_region
